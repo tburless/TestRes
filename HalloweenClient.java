39b3691079f5b2ed7e6b67a25d342fb9e2970f9e
Thomas Burless <burleta0@sewanee.edu>
2017-04-06 11:32:18 -0500

    This file shows a colorful skull

---
+++ HalloweenClient.java
@@ -0,0 +1,14 @@
+/Thomas Burless
+//CSCI 157 - Lab  9
+//10-14-16
+//Halloween themed client that creates pumpkins and implements
+//an interface
+import objectdraw.*;
+import java.awt.*;
+
+public class BorderFishClient extends WindowController
+                              implements Drawable157  {
+
+  public static void main(String[] args) {
+    new BorderFishClient().startController(800,900);
+  }
